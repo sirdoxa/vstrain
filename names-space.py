941ae184350106f2fe6cc2af9ee6def3ac588492
Mahdi <sirdoxa@gmail.com>
2025-02-16 22:42:18 +0330

    practicing on names-space

--- names-space.py
+++ names-space.py
@@ -1,3 +1,6 @@
+'''local --> enclosed --> global --> built-in'''
+
+
 def a():
     x = 12 
     print('a->',x)
@@ -6,4 +9,17 @@ def a():
         print('a2->',x)
     a2()
 a()    
-#a2() #not defined because we didn't call a()
+
+# a2() not defined because we didn't call a()
+
+#--------------------------------------------------
+
+x = 10 
+def g():
+    x = 15
+    def g2():
+        global x
+        x += 12
+        print(x)
+    g2()
+g()
