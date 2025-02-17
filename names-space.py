6b456708754ba493a4c84df1da64b1068b6d4faa
Mahdi <sirdoxa@gmail.com>
2025-02-17 09:59:06 +0330

    autopep8 files -->  shift+alt+F

--- names-space.py
+++ names-space.py
@@ -2,24 +2,32 @@
 
 
 def a():
-    x = 12 
-    print('a->',x)
+    x = 12
+    print('a->', x)
+
     def a2():
         x = 5
-        print('a2->',x)
+        print('a2->', x)
     a2()
-a()    
+
+
+a()
 
 # a2() not defined because we didn't call a()
 
-#--------------------------------------------------
+# --------------------------------------------------
 
-x = 10 
+x = 10
+
+
 def g():
     x = 15
+
     def g2():
         global x
         x += 12
         print(x)
     g2()
+
+
 g()
