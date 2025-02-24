4cab11bccf0d56ee64b70914e7cc39014f5dfdd6
Mahdi <sirdoxa@gmail.com>
2025-02-25 01:42:22 +0330

    some decorator excersises

--- decorator.py
+++ decorator.py
@@ -1,15 +1,56 @@
-def exmp(func):
-    def inner ():
-        print(15 * '*')
-        func()
+# Decorator 
+# توابع تو در تو
+# ارسال تابع به عنوان ارگومان
 
-        print(15 * '*') 
-    return inner
+# -------------------------------------------------
+
+# def exmp(func):
+#     def inner ():
+#         print(15 * '*')
+#         func()
+
+#         print(15 * '*') 
+#     return inner
 
 
-def func():
-    print ('dana haroom')
+# def func():
+#     print ('dana haroom')
 
-s = exmp(func)
+# s = exmp(func)()
+
+# print('***************\ntaha\nnabavi\n***************')
+
+# ------------------------------------------------------
+
+# def mo(m):
+#     def inner(h, j):
+#         if j == 0:
+#             return'warning!!!!!'
+#         return m(h, j)
+#     return inner
+
+# @mo   # @ + the name of decorator function can run python decorator function on our new function
+# def m(h, j):
+#     return h/j
+
+# print(m(10, 0))
+
+
+
+# =======================================
+
+def dec(ka):
+    def mo():
+        print('=' * 20)
+        ka()
+        print('=' * 20)
+    mo()
+    return mo
+
+
+
+def ka():
+    return 'mahdi'
+
+s = dec(ka)
 s()
-print('***************\ntaha\nnabavi\n***************')
