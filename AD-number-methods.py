e44e72ac9510363ef0d80ee73a35e56dc2ff785e
Mahdi <sirdoxa@gmail.com>
2025-03-02 00:37:45 +0330

    debugging

---
+++ AD-number-methods.py
@@ -0,0 +1,8 @@
+print(divmod(10,3)) # (x//y , x % y)
+print(pow(10,3)) # x ** y
+print(pow(2,3,5)) # x ** y % z
+print(round(3.56564156,3)) # 3.66564156 , x-->4.x
+print(abs(-55)) # -x --> +x
+#0b....-->2
+#0o....-->8
+#0x....-->16 
