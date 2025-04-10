ba3cac1230f055821513f7fbcb3000bb400a5d32
Mahdi <sirdoxa@gmail.com>
2025-04-10 23:09:12 +0330

    add something for learning modules using importlib and something else

--- module/mod1.py
+++ module/mod1.py
@@ -4,8 +4,22 @@ def f (x,y):
 def upper(x):
     print(x.upper())
 
-f(5,81)
-upper('mahdi')
+# f(5,81)
+# upper('mahdi')
+#
+# def po(x):
+#     print(x ** 2)
+from  functools import wraps
+def dec(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        print(10 * '*')
+        result = f(*args, **kwargs)
+        print(result)
+        print(10 * '*')
+    return wrapper
 
-def po(x):
-    print(x ** 2)
+@dec
+def dec_name():
+    p = input()
+    return p
---
+++ modulef.py
@@ -0,0 +1,9 @@
+def func1(x):
+    tot = 0
+    for i in x:
+        tot += int(i)
+    return tot
+
+
+name = 'Mahdi Nabavi'
+_age = '18'
