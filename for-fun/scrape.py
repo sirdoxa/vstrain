2fb9d65d293bcb046a0f6fc60fd330930295d448
Mahdi <sirdoxa@gmail.com>
2025-04-14 03:05:38 +0330

    edit scrape.py file

--- for-fun/scrape.py
+++ for-fun/scrape.py
@@ -1,47 +1,47 @@
 import requests
 from bs4 import BeautifulSoup
 
-def decorated_output(func):
+def dec(func):
     def wrapper():
         print('*' * 40)
         func()
         print('*' * 40)
     return wrapper
 
-def format_price(price):
+def fp(price):
     return "{:,}".format(int(price))
 
-@decorated_output
+@dec
 def dollar():
     response = requests.get("https://www.tgju.org/")
     soup = BeautifulSoup(response.text, 'html.parser')
     prices = soup.find_all('td', class_='market-price')
     dollar_price = prices[0].text.replace(',', '').strip()
-    print(f"Dollar price: {format_price(dollar_price)} IRR")
+    print(f"Dollar price: {fp(dollar_price)} IRR")
 
-@decorated_output
+@dec
 def euro():
     response = requests.get("https://www.tgju.org/")
     soup = BeautifulSoup(response.text, 'html.parser')
     prices = soup.find_all('td', class_='market-price')
     euro_price = prices[1].text.replace(',', '').strip()
-    print(f"Euro price: {format_price(euro_price)} IRR")
+    print(f"Euro price: {fp(euro_price)} IRR")
 
-@decorated_output
+@dec
 def pound():
     response = requests.get("https://www.tgju.org/")
     soup = BeautifulSoup(response.text, 'html.parser')
     prices = soup.find_all('td', class_='market-price')
     pound_price = prices[3].text.replace(',', '').strip()
-    print(f"Pound price: {format_price(pound_price)} IRR")
+    print(f"Pound price: {fp(pound_price)} IRR")
 
-@decorated_output
+@dec
 def gold():
     response = requests.get("https://www.tgju.org/")
     soup = BeautifulSoup(response.text, 'html.parser')
     prices = soup.find_all('td', class_='nf')
     gold_price = prices[18].text.replace(',', '').strip()
-    print(f"Second-hand gold price: {format_price(gold_price)} IRR")
+    print(f"Second-hand gold price: {fp(gold_price)} IRR")
 
 while True:
     print("\nSelect an option:")
