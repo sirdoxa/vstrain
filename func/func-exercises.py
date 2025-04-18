e27778d7ee7aeccbaccbb6e674583e15fbba971b
Mahdi <sirdoxa@gmail.com>
2025-04-18 16:06:36 +0330

    rename folder from def to func

---
+++ func/func-exercises.py
@@ -0,0 +1,81 @@
+# def len1():
+#     st = input('enter an str: ')
+#     k = 0
+#     for i in st:
+#         k += 1
+#     print(k)
+
+
+# len1()
+
+# -------------------------
+
+# def max1():
+#     st = input('enter some numbers: ').split(' ')
+#     print(st)
+#     x = len(st) - 1
+#     print(st[x])
+
+# max1()
+
+# ----!!!!!!!!!!!!!!!!!!!!!!!!!!---
+
+# def min1():
+#     st = input('x: ').split(' ')
+#     l = []
+#     for i in st:
+#         l.append(int(i))
+#     l = sorted(l)
+#     print(l[0])
+
+
+# min1()
+
+# ----------------------------------
+
+# def sum1():
+#     st = sorted(input('x: ').split(' '))
+#     x = 0
+#     for i in st:
+#         i = int(i)    # !!!!!!!!!!!!!
+#         x = x + i
+#     print(x)
+# sum1()
+
+# -------------------------------
+
+# def moraba():
+#     x = int(input('enter your number: '))
+#     for i in range(0, x):
+#         if i ** 2 == x:
+#             print('yes')
+#             break
+#     else: #اگه تابع بریک بخوره کلا از همه حلقه ها میره بیرون
+#         print('no')
+# moraba()
+
+# --------------------------------------
+
+# def price():
+#     x = int(input("enter the price: "))
+#     y = int(input("enter the discount: "))
+#     y = y /100
+#     return print(f'the final price is {x - (x * y)} dollars')
+# price()
+
+# --------------------------------
+
+# def find():
+#     x = input("enter chr: ")
+#     y = int(ord(x))
+#     if 57>=y>=48:
+#         print('its a number!!!')
+#     elif 90>=y>=65 :
+#         print('its a capital word!!!')
+#     elif 122>=y>=97 :
+#         print('its a small word!!!')
+#     else:
+#         print('others signs!!!')
+
+
+# find()
