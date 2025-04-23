d56da9ca57f183644c6949be7736ad2a4d4bcc8a
Mahdi <sirdoxa@gmail.com>
2025-04-23 20:57:51 +0330

    spammer for fun:)

--- for-fun/tel.py
+++ for-fun/tel.py
@@ -3,6 +3,6 @@ import time
 
 time.sleep(4)  
 for _ in range(1000):  
-    pyautogui.write('mahdi', interval=0.01)  
+    pyautogui.write('maman', interval=0.1)  
     pyautogui.press('enter')  
-
+    time.sleep(1)   
